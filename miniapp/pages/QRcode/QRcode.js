98f032033813413e42ab9703cec326c89760b12d
parsifal486 <mrliuzeyou@outlook.com>
2024-08-30 00:06:40 +0800

    add token in header of scanning request

--- miniapp/pages/QRcode/QRcode.js
+++ miniapp/pages/QRcode/QRcode.js
@@ -6,51 +6,57 @@ Page({
       productName: '高品质商品',
       location: '山东省青岛市崂山区小麦岛',
       time: '30',
-      temperature:'20',
+      temperature: '20',
       originX: '',
       originY: '',
       manager: '张三'
     }
   },
-  onLoad(){
+  onLoad() {
     const scanResult = wx.getStorageSync('scanResult');
     // console.log(scanResult);
-  
-    wx.request({
-      url: `${scanResult}`,
-      success:res=>{
-        console.log(res.data.data.records)
-        const record = res.data.data.records;
-        if(record.length>0){
-          const firstItem=record[0];
-          this.setData({
-            'product.productNumber': firstItem.commodityId || '',
-            'product.productName': firstItem.commodityPartition || '',
-            'product.location': firstItem.origin || '',
-            'product.time': firstItem.weedingTime || '',
-            'product.temperature': firstItem.temperature || '',
-            'product.originX': firstItem.originX || '',
-            'product.manager': firstItem.supervisor || '',
-            'product.originY': firstItem.originY || ''
-          })
-        }
-    
-    wx.request({
-      url: 'http://49.232.136.246:8090/wx/commodity/selectImgById',
-      data: {
-        commodityId: this.data.product.productNumber  // 使用传递的 commodityId
-        // commodityId:102900011006998
-      },
-      success:(res)=>{
-        
-        this.setData({
-          'product.imageUrl':res.data.data
+    wx.getStorage({
+      key: 'token',
+      success: res => {
+        wx.request({
+          url: `${scanResult}`,
+          header:{'token':res.data},
+          success: res => {
+            console.log(res.data.data.records)
+            const record = res.data.data.records;
+            if (record.length > 0) {
+              const firstItem = record[0];
+              this.setData({
+                'product.productNumber': firstItem.commodityId || '',
+                'product.productName': firstItem.commodityPartition || '',
+                'product.location': firstItem.origin || '',
+                'product.time': firstItem.weedingTime || '',
+                'product.temperature': firstItem.temperature || '',
+                'product.originX': firstItem.originX || '',
+                'product.manager': firstItem.supervisor || '',
+                'product.originY': firstItem.originY || ''
+              })
+            }
+
+            wx.request({
+              url: 'http://49.232.136.246:8090/wx/commodity/selectImgById',
+              data: {
+                commodityId: this.data.product.productNumber // 使用传递的 commodityId
+                // commodityId:102900011006998
+              },
+              success: (res) => {
+
+                this.setData({
+                  'product.imageUrl': res.data.data
+                })
+              }
+            })
+          }
         })
       }
     })
-      }
-    })
-    
+
+
   },
- 
+
 });
